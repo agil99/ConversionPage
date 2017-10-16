607aa864822e9ca2d61a7a72c70ca751dee2ef22
Alberto GIl <alberto8gil@gmail.com>
2017-10-16 09:50:11 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,10 +6,14 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
-@app.route("/response")
-def render_response():
-    gigaAmount = request.args["giga"]
-    reply = gigaAmount * 2000000000
-    return render_template("response.html", response = reply)
+@app.route("/giga")
+def render_giga():
+    if 'giga' in request.args:
+        gigaAmount = request.args["giga"]
+        reply = gigaAmount * 2000000000       
+        return render_template('gigaConversions.html', response = reply)
+    else: 
+        return render_templat('gigaConversions.html')
+    
 if __name__=="__main__":
     app.run(debug=False, port=54321)
